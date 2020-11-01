ae1ce2cfabfef193b98b50a56c64aa90fba97cb6
Ya-dola <Ya-dola@users.noreply.github.com>
2020-11-01 17:38:30 +0300

    Updated Logic of Player Movement with Joystick

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public FloatingJoystick floatingJoystick;
     public Rigidbody playerRigBody;
 
+    [Header("Joystick")]
+    [Range(0, 1)]
+    public float joystickDeadZone;
+    
+
     [Header("Player Speed")]
     public float playerSpeed;
     public int playerMinWalkingSpeed;
@@ -19,11 +24,17 @@ public class PlayerController : MonoBehaviour
     public float minCamDistance;
     public float maxCamDistance;
 
+    [Header("Enviroment")]
     public float sideWallDistance;
+
+    private float verticalJoystickValue;
+    private float horizontalJoystickValue;
 
     void Awake()
     {
         cameraController.cameraSpeed = playerMinWalkingSpeed;
+        verticalJoystickValue = 0;
+        horizontalJoystickValue = 0;
     }
 
     void Update()
@@ -41,16 +52,35 @@ public class PlayerController : MonoBehaviour
         if (GameManager.singleton.GameStarted)
             playerRigBody.MovePosition(transform.position + Vector3.forward * playerMinWalkingSpeed * Time.fixedDeltaTime);
 
-        Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        // General Movement of Player
+        if (floatingJoystick.Vertical > joystickDeadZone)
+            verticalJoystickValue = 1;
+        else if (floatingJoystick.Vertical < -joystickDeadZone)
+            verticalJoystickValue = -1;
+        else
+            verticalJoystickValue = 0;
+
+        if (floatingJoystick.Horizontal > joystickDeadZone)
+            horizontalJoystickValue = 1;
+        else if (floatingJoystick.Horizontal < -joystickDeadZone)
+            horizontalJoystickValue = -1;
+        else
+            horizontalJoystickValue = 0;
+
+        // Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        Vector3 direction = Vector3.forward * verticalJoystickValue + Vector3.right * horizontalJoystickValue;
         playerRigBody.AddForce(direction * playerSpeed * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
-        // To reset the force applied on the player and reduce the force applied on it gradually 
+        // To reset the force applied on the player and reducing it gradually 
         if (direction == Vector3.zero)
             playerRigBody.velocity = playerRigBody.velocity * playerSlowDownSpeed;
 
         // To stop applying force on the player after a threshold
         if (playerRigBody.velocity.magnitude < 0.1f)
             playerRigBody.velocity = Vector3.zero;
+
+        Debug.Log("Horizontal: " + floatingJoystick.Horizontal);
+        Debug.Log("Vertical: " + floatingJoystick.Vertical);
     }
 
     // Late Update used mainly for Camera Calculations and Calculations that need to occur after movement has occured
