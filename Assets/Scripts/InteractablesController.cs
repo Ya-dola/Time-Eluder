1c41c9c388fd9623d6aaee19675dfb4b9f025b71
Ya-dola <Ya-dola@users.noreply.github.com>
2020-11-07 17:43:50 +0300

    Adjustments to Interactables Collider Size

--- Assets/Scripts/InteractablesController.cs
+++ Assets/Scripts/InteractablesController.cs
@@ -4,55 +4,126 @@ using UnityEngine;
 
 public class InteractablesController : MonoBehaviour
 {
+    [Header("Interactables Y Position")]
+    public float yPos;
+
     [Header("Coin")]
     public GameObject coinPrefab;
     public Transform coinHolder;
+    public Vector3 coinColliderHalfSize;
 
     [Header("Roadblock")]
     public GameObject roadblockPrefab;
     public Transform roadblockHolder;
-    
+    public Vector3 roadblockColliderHalfSize;
+
     [Header("Damaged Roadblock")]
     public GameObject damagedRoadblockPrefab;
     public Transform damagedRoadblockHolder;
-    
+    public Vector3 damagedRoadblockColliderHalfSize;
+
     [Header("Left Laser Turret")]
     public GameObject leftLaserTurretPrefab;
     public Transform leftLaserTurretHolder;
-    
+
     [Header("Right Laser Turret")]
     public GameObject rightLaserTurretPrefab;
     public Transform rightLaserTurretHolder;
-
-    // Lists to keep track of interactable location within the game
-    [HideInInspector]
-    public List<GameObject> coinList;
-    [HideInInspector]
-    public List<GameObject> roadblockList;
-    [HideInInspector]
-    public List<GameObject> damagedRoadblockList;
-    [HideInInspector]
-    public List<GameObject> leftLaserTurretList;
-    [HideInInspector]
-    public List<GameObject> rightLaserTurretList;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Initialising the lists
-        coinList = new List<GameObject>();
-        roadblockList = new List<GameObject>();
-        damagedRoadblockList = new List<GameObject>();
-        leftLaserTurretList = new List<GameObject>();
-        rightLaserTurretList = new List<GameObject>();
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if (Physics.CheckBox(new Vector3(-2f, yPos, 22f), coinColliderHalfSize))
+        //     Debug.Log("Coin Colliding");
+        // if (Physics.CheckBox(new Vector3(-4f, yPos, 32f), roadblockColliderHalfSize, Quaternion.Euler(0f, 90f, 0)))
+        //     Debug.Log("Roadblock Colliding");
+        // if (Physics.CheckBox(new Vector3(-3f, yPos, 26f), damagedRoadblockColliderHalfSize, Quaternion.Euler(0f, 90f, 0)))
+        //     Debug.Log("Damaged Roadblock Colliding");
+    }
+
+    public void GenerateInteractables()
+    {
+        // Destroying Existing Interactables if any
+        DestroyExistingInteractables();
+
+        // Generate Coin Interactables
+        GenerateCoins();
+
+        // Generate Roadblock Interactables
+        GenerateRoadblocks();
+
+        // Generate Damaged Roadblock Interactables
+        GenerateDamagedRoadblocks();
+
+        // Generate Left Laser Turret Interactables
+        GenerateLeftLaserTurret();
+
+        // Generate Right Laser Turret Interactables
+        GenerateRightLaserTurret();
+
+    }
+
+    private void DestroyExistingInteractables()
+    {
+
+    }
+
+
+
+    private void GenerateCoins()
+    {
+        // int positionX;
+        // int positionZ;
+
+        // GameObject gameObject;
+
+        // for (int i = 0; i < size; i++)
+        // {
+        //     while (true)
+        //     {
+        //         positionX = UnityEngine.Random.Range(0, GridWidth);
+        //         positionZ = UnityEngine.Random.Range(0, GridHeight);
+
+        //         if (!IsCellOccupied(new Vector3(positionX, positionY, positionZ)))
+        //         {
+        //             gameObject = Instantiate(prefab, new Vector3(positionX, positionY, positionZ), Quaternion.identity, parent);
+        //             createdGameObjectsList.Add(gameObject);
+        //             break;
+        //         }
+        //         else
+        //         {
+        //             Debug.Log("Cell Occupied at: (" + positionX + ", " + positionY + ", " + positionZ + ") - Recreating: " + prefab.name);
+        //         }
+        //     }
+        // }
+    }
+    private void GenerateRoadblocks()
+    {
+
+    }
+    private void GenerateDamagedRoadblocks()
+    {
+
+    }
+    private void GenerateLeftLaserTurret()
+    {
+
+    }
+
+    private void GenerateRightLaserTurret()
+    {
+
+    }
+
+    private void isPosColliderExist()
+    {
+
     }
 }
