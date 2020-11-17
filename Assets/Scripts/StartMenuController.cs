c9ca0ffcc0ef657c326c0209e31265f600f95ccf
Ya-dola <Ya-dola@users.noreply.github.com>
2020-11-17 19:00:49 +0300

    Adjusted UI to Scale with Screen Size

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void StartTimeEluder()
+    {
+        // Scene Numbers are according to those shown in Build
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game Scene");
+    }
+
+    public void QuitTimeEluder()
+    {
+        // Written to show as Application.Quit doesnt do anything in Editor
+        Debug.Log("Quit the Game !!!");
+
+        Application.Quit();
+    }
+}
